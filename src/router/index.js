814fcb9bb3b581223765445902ab92361f315b59
xiaoz <liuzetan314@gmail.com>
2017-03-11 15:52:38 +0800

    animate example

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
+import Animate from '@/components/Animate'
 
 Vue.use(Router)
 
@@ -10,6 +11,10 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: Hello
+    }, {
+      path: '/animate',
+      name: 'animate',
+      component: Animate
     }
   ]
 })
